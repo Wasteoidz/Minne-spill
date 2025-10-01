9a4e50149e4b37f1ec3edf4f29965044a48756bb
Wasteoidz <Rolf91@live.com>
2025-10-01 10:33:23 +0200

    småplukk

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Model
 const spill = document.getElementById('app');
-const cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
+const cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20];
 
 
 
@@ -29,14 +29,17 @@ function getCards(times) {
      const shuffled = shuffle([...cards]);
     for (let i = 0; i<times; i++) {
     document.getElementById('board').innerHTML += /*html*/ `
-                <div class="card" onclick="showCard()">
-                her er det kort ${shuffled[i]}
-                </div>`;
+                                    <div class="card"
+                                    data-value="${shuffled[i]}" 
+                                    onclick="showCard(this)">
+                                    kort ${shuffled[i]}
+                                    </div>`;
     }
 }
 
-function showCard() { //viser kort med onclick-og sjekker om det er likt forrige kort
-    
+function showCard(kort) { //viser kort med onclick-og sjekker om det er likt forrige kort
+    const cardValue = kort.dataset.value;
+    console.log(cardValue);
 }
 
 // Fisher–Yates shuffle
