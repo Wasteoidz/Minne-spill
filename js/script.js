199ce3b13f2f3cd833865067e14e8c98680d5d65
Wasteoidz <Rolf91@live.com>
2025-10-01 12:18:29 +0200

    spill1 lagt til

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 //Model
 const spill = document.getElementById('app');
-const cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20];
-
+const decks = {
+    tall: [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20],
+    spill1: [],
+    spill2: [],
+}
 
 
 //viewer
@@ -13,7 +16,7 @@ function showView() {
             <h1 class="tempText" id="tempText">⁉️Kan du finne alle like⁉️</h1>
         </div> 
         <div class="knapper">
-            <button class="knapp" onclick=getCards(40)>Spill 1</button>
+            <button class="knapp" onclick=getCards(40, decks.tall)>Spill 1</button>
             <button class="knapp" onclick=getCards(40)>Spill 2</button>
             <button class="knapp" onclick=getCards(40)>Spill 2</button>
         </div>      
@@ -23,23 +26,22 @@ function showView() {
 
 
 //Controller
-function getCards(times) {
+function getCards(times, cardset) {
     showView();
     document.getElementById('tempText').style.display = "none";
-     const shuffled = shuffle([...cards]);
+    const shuffled = shuffle([...cardset]);
     for (let i = 0; i<times; i++) {
-    document.getElementById('board').innerHTML += /*html*/ `
-                                    <div class="card"
-                                    data-value="${shuffled[i]}" 
-                                    onclick="showCard(this)">
-                                    kort ${shuffled[i]}
-                                    </div>`;
+        document.getElementById('board').innerHTML += /*html*/ `
+            <div class="card" data-value="${shuffled[i]}" onclick="showCard(this)">
+                    <div class="card-front">❓</div> 
+                    <div class="card-back">${shuffled[i]}</div>
+            </div>`;
     }
 }
 
-function showCard(kort) { //viser kort med onclick-og sjekker om det er likt forrige kort
-    const cardValue = kort.dataset.value;
-    console.log(cardValue);
+function showCard(kort) {
+    kort.classList.toggle("flipped");
+    console.log("Valgt kort:", kort.dataset.value);
 }
 
 // Fisher–Yates shuffle
